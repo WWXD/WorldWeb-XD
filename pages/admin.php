d20364599ad3756a77c790ecdc0e143058301bb0
maorninja <maorninja@outlook.com>
2016-08-17 21:31:03 -0700

    Update admin.php
    New links

--- pages/admin.php
+++ pages/admin.php
@@ -26,20 +26,20 @@ $adminInfo[__('Last viewcount milestone')] = $misc['milestone'];
 
 $adminLinks = array();
 
-if ($loguser['root']) 						$adminLinks[] = actionLinkTag(__("Recalculate statistics"), "recalc");
+if ($loguser['root']) 				$adminLinks[] = actionLinkTag(__("Recalculate statistics"), "recalc");
 if (HasPermission('admin.manageipbans'))	$adminLinks[] = actionLinkTag(__("Manage IP bans"), "ipbans");
 if (HasPermission('admin.editforums'))		$adminLinks[] = actionLinkTag(__("Manage forum list"), "editfora");
-if (HasPermission('admin.editsettings'))
-{
-	$adminLinks[] = actionLinkTag(__("Manage plugins"), "pluginmanager");
-	$adminLinks[] = actionLinkTag(__("Edit settings"), "editsettings");
-	$adminLinks[] = actionLinkTag(__("Edit home page"), "editsettings", '', 'field=homepageText');
-	$adminLinks[] = actionLinkTag(__("Edit FAQ"), "editsettings", '', 'field=faqText');
-}
-if (HasPermission('admin.editsmilies'))		$adminLinks[] = actionLinkTag(__("Edit smilies"), "editsmilies");
-if ($loguser['root'])						$adminLinks[] = actionLinkTag(__("Optimize tables"), "optimize");
-if (HasPermission('admin.viewlog'))			$adminLinks[] = actionLinkTag(__("View log"), "log");
+if (HasPermission('admin.editsettings'))	$adminLinks[] = actionLinkTag(__("Manage plugins"), "pluginmanager");
+if (HasPermission('admin.editsettings'))	$adminLinks[] = actionLinkTag(__("Edit Settings"), "editsettings");
+if (HasPermission('admin.editsettings'))	$adminLinks[] = actionLinkTag(__("Edit Home Page"), "editsettings", '', 'field=homepageText');
+if (HasPermission('admin.editsettings'))	$adminLinks[] = actionLinkTag(__("Edit FAQ"), "editsettings", '', 'field=faqText');
+if (HasPermission('admin.editsmilies'))		$adminLinks[] = actionLinkTag(__("Manage smilies"), "editsmilies");
+if ($loguser['root'])				$adminLinks[] = actionLinkTag(__("Optimize tables"), "optimize");
+if (HasPermission('admin.viewlog'))		$adminLinks[] = actionLinkTag(__("View log"), "log");
 if (HasPermission('admin.ipsearch'))		$adminLinks[] = actionLinkTag(__('Rereg radar'), 'reregs');
+if (HasPermission('admin.ipsearch'))		$adminLinks[] = actionLinkTag(__('Last Known Browsers'), 'lastknownbrowsers');
+if ($loguser['root'])		                $adminLinks[] = actionLinkTag(__('Edit Groups'), 'editgroups');
+if ($loguser['root'])		                $adminLinks[] = actionLinkTag(__('Add secondary Groups'), 'secgroups');
 
 
 $bucket = "adminpanel"; include(BOARD_ROOT."lib/pluginloader.php");
