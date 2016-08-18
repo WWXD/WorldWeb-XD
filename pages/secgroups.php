db43b15e1bf1d8483eab0f6ae843133861cd380f
maorninja <maorninja@outlook.com>
2016-08-17 23:09:53 -0700

    Update secondary groups page.
    Basically makes it looks neat.

--- pages/secgroups.php
+++ pages/secgroups.php
@@ -9,9 +9,10 @@ if ($_POST['userid'] && $_POST['groupid'])
 		$_POST['userid'], $_POST['groupid']);
 }
 
-?>
+?><table><tr><th colspan="2" class="header1 center">Add secondary groups</th></tr>
 <form action="" method="POST">
-User ID: <input type="text" name="userid"><br>
-Group ID: <input type="text" name="groupid"><br>
-<input type="submit" value="Add">
+<tr class="cell2"><td>User ID</td><td><input type="text" name="userid"></td></tr>
+<tr class="cell1"><td>Group ID</td><td><input type="text" name="groupid"></td></tr>
+<tr><td colspan="2" class="cell2"><input type="submit" value="Add"></td></tr>
 </form>
+</table>
