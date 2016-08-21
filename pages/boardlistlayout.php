0a07e37f06c4b46bae45bdb4f054b713f4a9c33c
maorninja <maorninja@outlook.com>
2016-08-21 11:40:34 -0700

    Create boardlistlayout.php
    Listing all the layouts.

---
+++ pages/boardlistlayout.php
@@ -0,0 +1,3 @@
+<table><tr class="cell1"><td><a href="/?page=board">BlargBoard</a></td></tr>
+<tr class="cell2"><td><a href="/?page=board1"><?php if (!$mobileLayout) { ?>ACMLM Board<?php } else { ?>ABXD<?php } ?> </a></td></tr>
+<table><tr class="cell1"><td><a href="/?page=board2">Clustered</a></td></tr>
