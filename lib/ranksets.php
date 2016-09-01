51063fc834284f027c415601baa81c9202971b07
maorninja <maorninja@outlook.com>
2016-09-01 00:14:51 -0700

    Update ranksets.php
    Add JSON Support

--- lib/ranksets.php
+++ lib/ranksets.php
@@ -1,5 +1,4 @@
 <?php
-if (!defined('BLARG')) die();
 
 function loadRanksets()
 {
@@ -10,24 +9,52 @@ function loadRanksets()
 	$ranksetData = array();
 	$ranksetNames = array();
 
-	$dir = __DIR__."/../ranksets/";
+	$dir = "ranksets/";
 
-	if (is_dir($dir))
-	{
-		if ($dh = opendir($dir))
-		{
-		    while (($file = readdir($dh)) !== false)
-		    {
-		        if(filetype($dir . $file) != "dir") continue;
-		        if($file == ".." || $file == ".") continue;
-		        $infofile = $dir.$file."/rankset.php";
-
-		        if(file_exists($infofile))
-		        	include($infofile);
-		    }
-		    closedir($dh);
-		}
-	}
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                if(filetype($dir . $file) != "dir") continue;
+                if($file == ".." || $file == ".") continue;
+                $jsonfile = $dir.$file."/rankset.json";
+                if(file_exists($jsonfile)) {
+                    switch (json_last_error()) {
+                    case JSON_ERROR_NONE:
+                        break;
+                    case JSON_ERROR_DEPTH:
+                        echo ' - Maximum stack depth exceeded';
+                        break;
+                    case JSON_ERROR_STATE_MISMATCH:
+                        echo ' - Underflow or the modes mismatch';
+                        break;
+                    case JSON_ERROR_CTRL_CHAR:
+                        echo ' - Unexpected control character found';
+                        break;
+                    case JSON_ERROR_SYNTAX:
+                        echo ' - Syntax error, malformed JSON';
+                        break;
+                    case JSON_ERROR_UTF8:
+                        echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                        break;
+                    default:
+                        echo ' - Unknown JSON error';
+                        break;
+                    }
+                    $ranksetNames[$file] = $file;
+                    $ranksetData[$file] = array();
+                    $data = json_decode(file_get_contents($jsonfile, FILE_USE_INCLUDE_PATH), true);
+                    foreach($data as $text => $d) {
+                        $num = $d["num"];
+                        $image = $d["image"];
+                        array_push($ranksetData[$file],array("num" => $num, "image" => $image, "text" => $text));
+                    }
+                }
+                else if(file_exists($dir.$file."/rankset.php"))
+                    include($dir.$file."/rankset.php");
+            }
+            closedir($dh);
+        }
+    }
 }
 
 function getRankHtml($rankset, $rank)
