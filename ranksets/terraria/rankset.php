3ddc9bd6a789975b815261f1058115b6fc30ae89
maorninja <maorninja@outlook.com>
2016-09-11 08:58:06 -0700

    Fix terraria.

--- ranksets/terraria/rankset.php
+++ ranksets/terraria/rankset.php
@@ -21,7 +21,7 @@ array("num" => 150, "image" => 'Wizard.png', "text" => 'Wizard'),
 array("num" => 160, "image" => 'Arms_Dealer.png', "text" => 'Arms Dealer'), 
 array("num" => 170, "image" => 'Mechanic.png', "text" => 'Mechanic'), 
 array("num" => 180, "image" => 'Travelling_Merchant.png', "text" => 'Travelling Merchant'), 
-array("num" => 190, "image" => 'Witch_Doctor.png', "text" => 'Witch Doctor')
+array("num" => 190, "image" => 'Witch_Doctor.png', "text" => 'Witch Doctor'), 
 array("num" => 200, "image" => 'Guide.png', "text" => 'Guide'), 
 array("num" => 215, "image" => 'Demolitionist.png', "text" => 'Demolitionist'), 
 array("num" => 230, "image" => 'Truffle.png', "text" => 'Truffle'), 
