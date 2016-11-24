d69a76cb4cf8c5dd5cfe96709509985480fe3678
maorninja <maorninja@outlook.com>
2016-11-24 12:57:13 -0800

    Custom badges and noob tutorial.
    Its for newcomers to BBXD

--- pages/secgroups.php
+++ pages/secgroups.php
@@ -3,13 +3,19 @@ if (!defined('BLARG')) die();
 
 CheckPermission('admin.editusers');
 
-if ($_POST['userid'] && $_POST['groupid'])
-{
+if ($_POST['userid'] && $_POST['groupid']) {
 	Query("INSERT INTO {secondarygroups} (userid,groupid) VALUES ({0},{1})",
 		$_POST['userid'], $_POST['groupid']);
+	Alert(__("Secondary group successfully added."), __("Notice"));
+} else if (!$_POST['userid'] && $_POST['groupid']) {
+	Alert(__("Please enter a user ID and try again."), __("Notice"));
+} else if ($_POST['userid'] && !$_POST['groupid']) {
+	Alert(__("Please enter a group ID and try again."), __("Notice"));
+} else if (!$_POST['userid'] && !$_POST['groupid']) {
+	Alert(__("Please enter a Group ID and a User ID."), __("Notice"));
 }
 
-?><table><tr><th colspan="2" class="header1 center">Add secondary groups</th></tr>
+?><table class="outline"><tr class="header1"><th colspan="2" class="center">Add secondary groups</th></tr>
 <form action="" method="POST">
 <tr class="cell2"><td>User ID</td><td><input type="text" name="userid"></td></tr>
 <tr class="cell1"><td>Group ID</td><td><input type="text" name="groupid"></td></tr>
