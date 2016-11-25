884d4693f8c37215ac5ee700e4f06659534e66f7
maorninja <maorninja@outlook.com>
2016-11-25 11:45:52 -0800

    Add theme search

--- pages/editprofile.php
+++ pages/editprofile.php
@@ -684,7 +684,7 @@ asort($themes);
 
 $themeList .= "
 	<div style=\"text-align: right;\">
-		<input type=\"text\" placeholder=\"".__("Search")."\" id=\"search\" onkeydown=\"searchThemes(this.value);\" />
+		<input type=\"text\" placeholder=\"".__("Search")."\" id=\"search\" onkeyup=\"searchThemes(this.value);\" />
 	</div>";
 
 foreach($themes as $themeKey => $themeData) {
