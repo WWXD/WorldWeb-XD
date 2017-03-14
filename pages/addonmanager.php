334e4887124466fc13396a2476546b381210a5e9
Repflez <repflez@gmail.com>
2017-03-13 21:36:20 -0700

    Fix addons not installing.

--- pages/addonmanager.php
+++ pages/addonmanager.php
@@ -13,16 +13,16 @@ if($_REQUEST['action'] == "enable") {
 	if($_REQUEST['key'] != $loguser['token'])
 		Kill("No.");
 
-	Query("insert into {enabledplugins} values ({0})", $_REQUEST['id']);
+	Query("insert into {enabledplugins} values ({0})", $_REQUEST['addon']);
 	require(BOARD_ROOT.'db/functions.php');
 	Upgrade();
 
 	//Make a new file for easier detecting that it is enabled
 	if (!file_put_contents($enabledfile, 'This is a holdertext file that signifies that this add-on is enabled. Don\'t delete this file.')) {
-		Report("[b]".$loguser['name']."[/] tried to add a add-on called ".$_REQUEST['id']." but failed.", false);
+		Report("[b]".$loguser['name']."[/] tried to add a add-on called ".$_REQUEST['addon']." but failed.", false);
 		Alert(__("Sorry, but the add-on couldn't be added by our file detection usage. Please report this to the website's owner."), __("Error"));
 	} else {
-		Report("[b]".$loguser['name']."[/] successfully added an add-on called ".$_REQUEST['id'].".", false);
+		Report("[b]".$loguser['name']."[/] successfully added an add-on called ".$_REQUEST['addon'].".", false);
 		Alert(__("You have successfully added the add-on."), __("Success"));
 	}
 
@@ -33,15 +33,15 @@ if($_REQUEST['action'] == "disable") {
 	if($_REQUEST['key'] != $loguser['token'])
 		Kill("No.");
 
-	Query("delete from {enabledplugins} where plugin={0}", $_REQUEST['id']);
+	Query("delete from {enabledplugins} where plugin={0}", $_REQUEST['addon']);
 
 	//Delete the enabled text.
 	if (file_exists($enabledfile)) {
 		if(!unlink($enabledfile)) {
-			Report("[b]".$loguser['name']."[/] tried to remove a add-on called ".$_REQUEST['id']." but failed.", false);
+			Report("[b]".$loguser['name']."[/] tried to remove a add-on called ".$_REQUEST['addon']." but failed.", false);
 			Alert(__("Sorry, but the add-on couldn't be removed by our file detection usage. Please report this to the website's owner."), __("Error"));
 		} else {
-			Report("[b]".$loguser['name']."[/] successfully removed an add-on called ".$_REQUEST['id'].".", false);
+			Report("[b]".$loguser['name']."[/] successfully removed an add-on called ".$_REQUEST['addon'].".", false);
 			Alert(__("You have successfully removed the add-on."), __("Success"));
 		}
 	}
@@ -111,11 +111,11 @@ function listPlugin($plugin, $plugindata) {
 		$text = __("Disable");
 		$act = "disable";
 	}
-	$pdata['actions'] = '<ul class="pipemenu">'.actionLinkTagItem($text, "addonmanager", $plugin, "action=".$act."&key=".$loguser['token']);
+	$pdata['actions'] = '<ul class="pipemenu">'.actionLinkTagItem($text, "addonmanager", '', "addon=".$plugin."&action=".$act."&key=".$loguser['token']);
 
 	if(in_array("settingsfile", $plugindata['buckets'])) {
 		if(isset($plugins[$plugin]))
-			$pdata['actions'] .= actionLinkTagItem(__("Settings&hellip;"), "editsettings", $plugin);
+			$pdata['actions'] .= actionLinkTagItem(__("Settings&hellip;"), "editsettings", '', 'addon='.$plugin);
 	}
 	$pdata['actions'] .= '</ul>';
 
