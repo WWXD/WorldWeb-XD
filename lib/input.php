b2daf8094afe88b0f4e3669bfc22b9e1037dfaea
Repflez <repflez@gmail.com>
2017-03-20 12:23:09 -0700

    Allow using request from the input class

--- lib/input.php
+++ lib/input.php
@@ -1,34 +1,29 @@
 <?php
 
-class Input
-{
+class Input {
 	private $_post;
 	private $_get;
-	private $_session;
-	private $_server;
+	private $_request;
 
-	public function __construct()
-	{
+	public function __construct() {
 		$this->_post = $_POST;
 		$this->_get = $_GET;
-		#$this->_session = $_SESSION;
-		$this->_server = $_SERVER;
+		$this->_request = $_REQUEST;
 	}
 
-	public function post($key = null, $default = null)
-	{
+	public function post($key = null, $default = null) {
 		return $this->checkGlobal($this->_post, $key, $default);
 	}
 
-	public function get($key = null, $default = null)
-	{
+	public function get($key = null, $default = null) {
 		return $this->checkGlobal($this->_get, $key, $default);
 	}
 
-	// other accessors
+	public function request($key = null, $default = null) {
+		return $this->checkGlobal($this->_request, $key, $default);
+	}
 
-	private function checkGlobal($global, $key = null, $default = null)
-	{
+	private function checkGlobal($global, $key = null, $default = null) {
 		if ($key) {
 			if (isset($global[$key])) {
 				return $global[$key];
