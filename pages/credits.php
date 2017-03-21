17e59a424a7483e272272f1b573ab5fd44b9ac37
maorninja <maorninja@outlook.com>
2017-03-21 08:20:22 -0700

    Update credits.php
    Add new info about Repflez, fix Super Toad65's name, etc...

--- pages/credits.php
+++ pages/credits.php
@@ -10,8 +10,8 @@ $credits = ('
 		<li>Maorninja322 — Created WorldWeb XD (a fork of Blargboard), coder</li>
 		<li>JeDaYoshi — Ported all the Acmlmboard XD plugins to Blargboard, ported the RPG, coder</li>
 		<li>Phase — Added compatibility to 5.7 MySQL databases, JSON ranksets, etc...</li>
-		<li>MoonlightCapital / Super-toad 64 - (TBH: I don\'t know exactly what he did and what he didn\'t so I\'ll just leave it like this).</li>
-		<li>Repflez - Fixed bugs, and made a private fork of Blargboard, which is going to get merged here soon.</li>
+		<li>MoonlightCapital / Super-toad 65 - (Did not enter anything here. @Super Toad65, please enter something.).</li>
+		<li>Repflez - Fixed bugs, and made a private fork of Blargboard, which got merged here, did a full URL rewritting system, etc...</li>
 		<li>DankMemeItTheFrog - Made Instameme, and ported the Layout Maker, while adding some of his changes.</li>
 		<li>LifeMushroom — Themes</li>
 		<li>Everyone behind <a href="https://fortawesome.github.io/Font-Awesome/">Font Awesome</a>, <a href="https://jquery.com/">jQuery</a>, Smarty, and any other libraries this software uses</li>
