ff479c81de32a85072c31ebad9be5372c55f5ac8
MoonlightCapital <lop-2013@libero.it>
2017-04-03 14:35:05 +0200

    the usual mistypos...

--- pages/credits.php
+++ pages/credits.php
@@ -1,16 +1,17 @@
 <?php
 if (!defined('BLARG')) die();
 ?>
+
 <table class="outline">
 	<tr class="header0"><th>Credits</th></tr>
 	<tr class="cell0"><td>
 		This board software was created by the following contributors, in no special order.<br>
 	<ul>
 		<li>Kawa — Originally created Acmlmboard XD ("ABXD")</li>
-		<li>Dirabio — Contributed to ABXD 3.0, which parts of this board are based off of</li>
+		<li>Dirbaio — Contributed to ABXD 3.0, which parts of this board are based off of</li>
 		<li>StapleButter — Created Blargboard (a fork of Acmlmboard XD)</li>
 		<li>Maorninja322 — Created WorldWeb XD (a fork of Blargboard), coder</li>
-		<li>SuperToad 65 — Made a password generator.</li>
+		<li>Super-toad 65/MoonlightCapital — Made a password generator.</li>
 		<li>JeDaYoshi — Ported all the Acmlmboard XD plugins to Blargboard, ported the RPG, coder</li>
 		<li>Phase — Added compatibility to 5.7 MySQL databases, JSON ranksets, etc...</li>
 		<li>Repflez - Fixed bugs, and made a private fork of Blargboard, which got merged here, did a full URL rewritting system, etc...</li>
