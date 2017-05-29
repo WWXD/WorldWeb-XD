cd8aed9aea9348de5708ed702ceb8c36f253e0d1
EzioisAwesome56 <jedijon567@gmail.com>
2017-05-29 12:13:01 -0400

    add link to list
    not sure if I can use action link for this, but oh well

--- plugins/IRC/pages/irc.php
+++ plugins/IRC/pages/irc.php
@@ -40,5 +40,4 @@ Port: 6667<br><p>If your IRC client allows irc:// links, click <a href="irc://<?
 	There are hundreds of IRC channels (discussion areas) around the world, hosted on servers, on which people type their messages to others on the same channel interested in the same subject. 
 	There are client IRC programs which provide graphical interfaces which make it easier for people log on and access active channels and send and receive the messages.<br><br>
 	
-A well-known cross-platform Graphical IRC client is HexChat.<br>
-You can get a <a href="https://hexchat.github.io/downloads.html">Windows</a> version, and for linux you can check in your package respository. For Mac OS x, use Xchat aqua/azure, get it from either <a href="http://itunes.apple.com/app/id447521961">the app store</a> or <a href="https://xchataqua.github.io/">from their offical site</a></td></tr></table>
+click <?php actionlink('ircclients') => __("here") ?> for a list of clients for most platforms</td></tr></table>
