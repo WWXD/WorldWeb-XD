bb09e806080a9a7c751e88e994317813a8900872
EzioisAwesome56 <jedijon567@gmail.com>
2017-05-29 11:48:58 -0400

    Xchat sucks (see desc.)
    Xchat for windows makes you pay for it after a month or so, there is a fork of xchat called hexchat which is free and better, its also for linux
    
    for macs, I just updated the links and reworded it slightly

--- plugins/IRC/pages/irc.php
+++ plugins/IRC/pages/irc.php
@@ -40,5 +40,5 @@ Port: 6667<br><p>If your IRC client allows irc:// links, click <a href="irc://<?
 	There are hundreds of IRC channels (discussion areas) around the world, hosted on servers, on which people type their messages to others on the same channel interested in the same subject. 
 	There are client IRC programs which provide graphical interfaces which make it easier for people log on and access active channels and send and receive the messages.<br><br>
 	
-A well-known cross-platform Graphical IRC client is X-Chat.<br>
-You can get a <a href="http://xchat.org/downloads/">Linux</a> (or check in your local package repository), <a href="http://www.uplinklabs.net/~tycho/projects/xchat-aqua/">Mac</a>, or <a href="http://www.silverex.org/download/">Windows</a> version.</td></tr></table>
+A well-known cross-platform Graphical IRC client is HexChat.<br>
+You can get a <a href="https://hexchat.github.io/downloads.html">Windows</a> version, and for linux you can check in your package respository. For Mac OS x, use Xchat aqua/azure, get it from either <a href="http://itunes.apple.com/app/id447521961">the app store</a> or <a href="https://xchataqua.github.io/">from their offical site</a></td></tr></table>
