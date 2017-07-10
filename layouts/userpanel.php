886389739ce01626e7108b8bc9ad501f475c37f9
maorninja <maorninja@outlook.com>
2017-07-09 19:26:46 -0700

    Re-implement the forums.
    Good bye forums plugin. You shall not be missed.

--- layouts/userpanel.php
+++ layouts/userpanel.php
@@ -3,16 +3,15 @@ if (!defined('BLARG')) die();
 
 $userMenu = [];
 
-if($loguserid)
-{
-	if (HasPermission('user.editprofile'))
-	{
+if($loguserid) {
+	if (HasPermission('user.editprofile')) {
 		$userMenu[actionLink('editprofile')] = __('Edit profile');
 		if (HasPermission('user.editavatars'))
 			$userMenu[actionLink('editavatars')] = __('Mood avatars');
 	}
 	
 	$userMenu[actionLink('private')] = __('Private messages');
+	$userMenu[actionLink('favorites')] = __('Favorites');
 
 	if (HasPermission('admin.viewadminpanel'))
 		$userMenu[pageLink('admin')] = __('Dashboard');
--- plugins/forums/adminpanel.php
+++
@@ -1,4 +0,0 @@
-<?php 
-if (HasPermission('admin.editforums')) {
-	$adminLinks[] = actionLinkTag(__("Manage forum list"), "editfora");
-}
